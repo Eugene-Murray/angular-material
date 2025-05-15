73f5672cc76ae7c4c04b5b13c710c7730033a7e5
Eugene Murray <eugene.murray@gmail.com>
2025-05-15 15:44:36 +0100

    add more

--- src/app/components/radio-button/radio-button.component.ts
+++ src/app/components/radio-button/radio-button.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { MatRadioModule } from '@angular/material/radio';
 
 @Component({
   selector: 'app-radio-button',
-  imports: [],
+  imports: [MatRadioModule, FormsModule],
   templateUrl: './radio-button.component.html',
   styleUrl: './radio-button.component.scss'
 })
 export class RadioButtonComponent {
-
+  favoriteSeason: string | undefined;
+  seasons: string[] = ['Winter', 'Spring', 'Summer', 'Autumn'];
 }
