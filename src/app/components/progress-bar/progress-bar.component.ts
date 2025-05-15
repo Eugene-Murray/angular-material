025123aa2881c933c3a5de9a55b29783b89af7d5
Eugene Murray <eugene.murray@gmail.com>
2025-05-15 15:42:21 +0100

    add more

--- src/app/components/progress-bar/progress-bar.component.ts
+++ src/app/components/progress-bar/progress-bar.component.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { MatCardModule } from '@angular/material/card';
+import { MatProgressBarModule, ProgressBarMode } from '@angular/material/progress-bar';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatSliderModule } from '@angular/material/slider';
 
 @Component({
   selector: 'app-progress-bar',
-  imports: [],
+  imports: [MatCardModule, MatRadioModule, FormsModule, MatSliderModule, MatProgressBarModule],
   templateUrl: './progress-bar.component.html',
   styleUrl: './progress-bar.component.scss'
 })
 export class ProgressBarComponent {
-
+  mode: ProgressBarMode = 'determinate';
+  value = 50;
+  bufferValue = 75;
 }
--- src/app/components/progress-spinner/progress-spinner.component.ts
+++ src/app/components/progress-spinner/progress-spinner.component.ts
@@ -1,11 +1,17 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { MatCardModule } from '@angular/material/card';
+import { MatProgressSpinnerModule, ProgressSpinnerMode } from '@angular/material/progress-spinner';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatSliderModule } from '@angular/material/slider';
 
 @Component({
   selector: 'app-progress-spinner',
-  imports: [],
+  imports: [MatCardModule, MatRadioModule, FormsModule, MatSliderModule, MatProgressSpinnerModule],
   templateUrl: './progress-spinner.component.html',
   styleUrl: './progress-spinner.component.scss'
 })
 export class ProgressSpinnerComponent {
-
+  mode: ProgressSpinnerMode = 'determinate';
+  value = 50;
 }
