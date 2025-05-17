2e650317b685cd7e428e20357313fffb1d482788
Eugene Murray <eugene.murray@gmail.com>
2025-05-17 15:27:26 +0100

    add more cdk stuff

--- src/app/components/_cdk/accordion/accordion.component.ts
+++ src/app/components/_cdk/accordion/accordion.component.ts
@@ -1,11 +1,13 @@
+import { CdkAccordionModule } from '@angular/cdk/accordion';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-accordion',
-  imports: [],
+  imports: [CdkAccordionModule],
   templateUrl: './accordion.component.html',
   styleUrl: './accordion.component.scss'
 })
 export class AccordionComponent {
-
+  items = ['Item 1', 'Item 2', 'Item 3', 'Item 4', 'Item 5'];
+  expandedIndex = 0;
 }
--- src/app/components/_cdk/bidirectionality/bidirectionality.component.ts
+++ src/app/components/_cdk/bidirectionality/bidirectionality.component.ts
@@ -1,11 +1,33 @@
-import { Component } from '@angular/core';
+import { Directionality } from '@angular/cdk/bidi';
+import { Component, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-bidirectionality',
   imports: [],
   templateUrl: './bidirectionality.component.html',
-  styleUrl: './bidirectionality.component.scss'
+  styleUrl: './bidirectionality.component.scss',
 })
-export class BidirectionalityComponent {
+export class BidirectionalityComponent implements OnDestroy {
+  /** Whether the widget is in RTL mode or not. */
+  private isRtl: boolean;
 
+  /** Subscription to the Directionality change EventEmitter. */
+  private _dirChangeSubscription = Subscription.EMPTY;
+
+  constructor(dir: Directionality) {
+    this.isRtl = dir.value === 'rtl';
+
+    this._dirChangeSubscription = dir.change.subscribe(() => {
+      this.flipDirection();
+    });
+  }
+
+  ngOnDestroy() {
+    this._dirChangeSubscription.unsubscribe();
+  }
+
+  flipDirection() {
+    console.log('Direction changed');
+  }
 }
