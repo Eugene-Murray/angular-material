02eaa0de556bd8f2b03e602533762ed25ddb29f0
Eugene Murray <eugene.murray@gmail.com>
2025-05-17 12:27:19 +0100

    add more

--- src/app/components/slide-toggle/slide-toggle.component.ts
+++ src/app/components/slide-toggle/slide-toggle.component.ts
@@ -1,11 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { _MatSlideToggleRequiredValidatorModule, MatSlideToggleModule } from '@angular/material/slide-toggle';
 
 @Component({
   selector: 'app-slide-toggle',
-  imports: [],
+  imports: [
+    MatSlideToggleModule,
+    FormsModule,
+    _MatSlideToggleRequiredValidatorModule,
+    MatButtonModule,
+    ReactiveFormsModule,
+  ],
   templateUrl: './slide-toggle.component.html',
   styleUrl: './slide-toggle.component.scss'
 })
 export class SlideToggleComponent {
+  private _formBuilder = inject(FormBuilder);
 
+  isChecked = true;
+  formGroup = this._formBuilder.group({
+    enableWifi: '',
+    acceptTerms: ['', Validators.requiredTrue],
+  });
+
+  alertFormValues(formGroup: FormGroup) {
+    alert(JSON.stringify(formGroup.value, null, 2));
+  }
 }
