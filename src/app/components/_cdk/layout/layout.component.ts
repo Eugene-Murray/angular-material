6d05150ab28f86a4c6a605b14bf4a158c9894cc6
Eugene Murray <eugene.murray@gmail.com>
2025-05-19 14:47:48 +0100

    add drag / drop

--- src/app/components/_cdk/layout/layout.component.ts
+++ src/app/components/_cdk/layout/layout.component.ts
@@ -1,11 +1,24 @@
-import { Component } from '@angular/core';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { Component, DestroyRef, OnInit } from '@angular/core';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { NavComponent } from './nav/nav.component';
 
 @Component({
   selector: 'app-layout',
-  imports: [],
+  imports: [NavComponent],
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.scss'
 })
-export class LayoutComponent {
+export class LayoutComponent implements OnInit{
+  message = 'Default message';
+
+  constructor(private breakpointObserver: BreakpointObserver, private destroyRef: DestroyRef) {}
+
+  ngOnInit() {
+    this.breakpointObserver.observe([Breakpoints.Handset])
+      .subscribe(result => {
+        this.message = result.matches ? 'Handset view' : 'Desktop view';
+      });
+  }
 
 }
