f4e37ed7078e6fcac0d6887dce25c099b46b483b
Eugene Murray <eugene.murray@gmail.com>
2025-05-19 13:54:46 +0100

    add text-field

--- src/app/components/_cdk/text-field/text-field.component.ts
+++ src/app/components/_cdk/text-field/text-field.component.ts
@@ -1,11 +1,16 @@
+import { TextFieldModule } from '@angular/cdk/text-field';
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-text-field',
-  imports: [],
+  imports: [MatFormFieldModule, MatInputModule, TextFieldModule, MatButtonModule],
   templateUrl: './text-field.component.html',
   styleUrl: './text-field.component.scss'
 })
 export class TextFieldComponent {
-
+  firstNameAutofilled: boolean | undefined;
+  lastNameAutofilled: boolean | undefined;
 }
